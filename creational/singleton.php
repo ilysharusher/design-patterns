61875690884ca54a88c40c4923eaa282674d13f3
Ilya <illyaetoilysha@gmail.com>
2024-02-04 18:57:32 +0200

    singleton is done

---
+++ creational/singleton.php
@@ -0,0 +1,40 @@
+<?php
+
+final class Connection
+{
+    private static self $instance;
+    public static string $name;
+
+    public function __clone(): void
+    {
+    }
+
+    public function __wakeup(): void
+    {
+    }
+
+    public static function getInstance(): self
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public static function getName(): string
+    {
+        return self::$name;
+    }
+
+    public static function setName(string $name): void
+    {
+        self::$name = $name;
+    }
+}
+
+$connection1 = Connection::getInstance();
+$connection1::setName('First Connection');
+echo $connection1::getName() . PHP_EOL;
+$connection2 = Connection::getInstance();
+echo $connection2::getName() . PHP_EOL;
