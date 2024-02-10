f67171b68dfa08e680e5a080c3e4febd020f1488
Ilya <illyaetoilysha@gmail.com>
2024-02-10 19:32:19 +0200

    bridge is done

---
+++ structural/bridge.php
@@ -0,0 +1,51 @@
+<?php
+
+interface Formatter
+{
+    public function format(string $text): string;
+}
+
+class PlainTextFormatter implements Formatter
+{
+    public function format(string $text): string
+    {
+        return $text;
+    }
+}
+
+class HtmlFormatter implements Formatter
+{
+    public function format(string $text): string
+    {
+        return "<p>$text</p>";
+    }
+}
+
+abstract class BridgeService
+{
+    public function __construct(
+        public Formatter $formatter
+    ) {
+    }
+
+    abstract public function show(string $text): string;
+}
+
+class BridgeServiceA extends BridgeService
+{
+    public function show(string $text): string
+    {
+        return $this->formatter->format($text);
+    }
+}
+
+class BridgeServiceB extends BridgeService
+{
+    public function show(string $text): string
+    {
+        return $this->formatter->format($text);
+    }
+}
+
+$serviceA = new BridgeServiceA(new PlainTextFormatter());
+echo $serviceA->show('Hello, World!');
